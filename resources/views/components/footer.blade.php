a916fb76037a329c9bde4b78b9250a70d9451767
roberto <robertoramirezmoreno@gmail.com>
2023-10-06 12:28:29 +0100

    hh

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,16 +5,7 @@
                 <h4>Sobre Noro News</h4>
                 <p>Esta plataforma nace para dar voz a temas marginados por la corrupción mediática y política, buscando destacar lo subyacente y relevante que a menudo se pasa por alto.</p>
             </div>
-            <div class="col-md-4">
-                <h4>Categorías</h4>
-                <ul class="list-unstyled">
-                    <li><a href="{{ route('ads.index', ['category' => 'espana']) }}">España</a></li>
-                    <li><a href="{{ route('ads.index', ['category' => 'Agenda2030']) }}">Agenda2030</a></li>
-                    <li><a href="{{ route('ads.index', ['category' => 'inmigracion']) }}">Fronteras abiertas</a></li>
-                    <li><a href="{{ route('ads.index', ['category' => 'autoritarismo']) }}">Autoritarismo</a></li>
-                    <li><a href="{{ route('ads.index', ['category' => 'lgtbiq+']) }}">Lgtbiq+</a></li>
-                </ul>
-            </div>
+         
             <div class="col-md-4">
                 <h4>Contacta con nosotros</h4>
                 <p>Email: beanroka@gmail.com</p>
